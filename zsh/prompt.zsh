67af44d3d624fb2e83c08516e6af67d75501beef
Lee Ourand <lee.ourand@vts.com>
2021-08-03 11:03:47 -0400

    ???

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,73 +1,75 @@
-autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+# autoload colors && colors
+# # cheers, @ehrenmurdick
+# # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
+# if (( $+commands[git] ))
+# then
+  # git="$commands[git]"
+# else
+  # git="/usr/bin/git"
+# fi
 
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
+# git_branch() {
+  # echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+# }
 
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
+# git_dirty() {
+  # if $(! $git status -s &> /dev/null)
+  # then
+    # echo ""
+  # else
+    # if [[ $($git status --porcelain) == "" ]]
+    # then
+      # echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+    # else
+      # echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+    # fi
+  # fi
+# }
 
 git_prompt_info () {
  ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
  echo "${ref#refs/heads/}"
 }
 
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
+# unpushed () {
+  # $git cherry -v @{upstream} 2>/dev/null
+# }
 
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
+# need_push () {
+  # if [[ $(unpushed) == "" ]]
+  # then
+    # echo " "
+  # else
+    # echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+  # fi
+# }
 
-directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
-}
+# directory_name() {
+  # echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+# }
 
-battery_status() {
-  if test ! "$(uname)" = "Darwin"
-  then
-    exit 0
-  fi
+# battery_status() {
+  # if test ! "$(uname)" = "Darwin"
+  # then
+    # exit 0
+  # fi
 
-  if [[ $(sysctl -n hw.model) == *"Book"* ]]
-  then
-    $ZSH/bin/battery-status
-  fi
-}
+  # if [[ $(sysctl -n hw.model) == *"Book"* ]]
+  # then
+    # $ZSH/bin/battery-status
+  # fi
+# }
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
-}
 
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
+# export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+# set_prompt () {
+  # export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+# }
+
+# precmd() {
+  # title "zsh" "%m" "%55<...<%~"
+  # set_prompt
+# }
+
+eval "$(starship init zsh)"
