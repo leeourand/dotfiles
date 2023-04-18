d3c13cbdeed7a92420f8fc993a111c203c2ff345
Lee Ourand <lee.ourand@vts.com>
2023-04-18 16:57:41 -0400

    ??

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias reload!='. ~/.zshrc'
 alias grep='grep --color'
+alias vim='nvim'
--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# one password autocompletion
+# eval "$(op completion zsh)"; compdef _op op
