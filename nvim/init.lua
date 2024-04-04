3f3a58d6547fd066ec97d1ac837a312b3eed5bae
Lee Ourand <lee@ourand.me>
2024-04-04 10:20:47 -0400

    :)

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,7 +25,7 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<leader>c', ':let @+ = expand("%")<CR>', { desc = 'Copy current file path to clipboard' })
+vim.keymap.set('n', '<leader>C', ':let @+ = expand("%")<CR>', { desc = 'Copy current file path to clipboard' })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
